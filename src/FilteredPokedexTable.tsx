68e4ce0849f277308eb8d02aa13c3f5c0b9c53e9
Felipe Flores <feliperflores2@hotmail.com>
2024-07-10 20:29:29 -0300

    Update FilteredPokedexTable.tsx

--- src/FilteredPokedexTable.tsx
+++ src/FilteredPokedexTable.tsx
@@ -3,63 +3,6 @@ import PokedexTable from './PokedexTable';
 import { DetailedPokemonApi, PokemonApi, PokemonType, SortOptions } from './PokemonTypes';
 import PokemonFilters from './PokemonFilters';
 
-// const pokemonArray: Pokemon[] = [
-//   {
-//     id: 1,
-//     name: 'Bulbasaur',
-//     types: ['Grass', 'Poison'],
-//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png',
-//   },
-//   {
-//     id: 2,
-//     name: 'Ivysaur',
-//     types: ['Grass', 'Poison'],
-//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/002.png',
-//   },
-//   {
-//     id: 3,
-//     name: 'Venusaur',
-//     types: ['Grass', 'Poison'],
-//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png',
-//   },
-//   {
-//     id: 4,
-//     name: 'Charmander',
-//     types: ['Fire'],
-//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png',
-//   },
-//   {
-//     id: 5,
-//     name: 'Charmeleon',
-//     types: ['Fire'],
-//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/005.png',
-//   },
-//   {
-//     id: 6,
-//     name: 'Charizard',
-//     types: ['Fire', 'Flying'],
-//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/006.png',
-//   },
-//   {
-//     id: 7,
-//     name: 'Squirtle',
-//     types: ['Water'],
-//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png',
-//   },
-//   {
-//     id: 8,
-//     name: 'Wartortle',
-//     types: ['Water'],
-//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/008.png',
-//   },
-//   {
-//     id: 9,
-//     name: 'Blastoise',
-//     types: ['Water'],
-//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/009.png',
-//   },
-// ];
-
 const FilteredPokedexTable = () => {
   const defaultPokemon = useRef<DetailedPokemonApi[]>([]);
   const [pokemon, setPokemon] = useState<DetailedPokemonApi[]>(defaultPokemon.current);
