5b4f372ba0a21be0497db41b3003fb1f4d279ebd
Felipe Flores <feliperflores2@hotmail.com>
2024-07-09 21:40:31 -0300

    Add start of API fetch

--- src/FilteredPokedexTable.tsx
+++ src/FilteredPokedexTable.tsx
@@ -1,76 +1,89 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PokedexTable from './PokedexTable';
 import { Pokemon, PokemonType, SortOptions } from './PokemonTypes';
 import PokemonFilters from './PokemonFilters';
 
-const pokemonArray: Pokemon[] = [
-  {
-    id: 1,
-    name: 'Bulbasaur',
-    types: ['Grass', 'Poison'],
-    sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png',
-  },
-  {
-    id: 2,
-    name: 'Ivysaur',
-    types: ['Grass', 'Poison'],
-    sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/002.png',
-  },
-  {
-    id: 3,
-    name: 'Venusaur',
-    types: ['Grass', 'Poison'],
-    sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png',
-  },
-  {
-    id: 4,
-    name: 'Charmander',
-    types: ['Fire'],
-    sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png',
-  },
-  {
-    id: 5,
-    name: 'Charmeleon',
-    types: ['Fire'],
-    sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/005.png',
-  },
-  {
-    id: 6,
-    name: 'Charizard',
-    types: ['Fire', 'Flying'],
-    sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/006.png',
-  },
-  {
-    id: 7,
-    name: 'Squirtle',
-    types: ['Water'],
-    sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png',
-  },
-  {
-    id: 8,
-    name: 'Wartortle',
-    types: ['Water'],
-    sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/008.png',
-  },
-  {
-    id: 9,
-    name: 'Blastoise',
-    types: ['Water'],
-    sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/009.png',
-  },
-];
+// const pokemonArray: Pokemon[] = [
+//   {
+//     id: 1,
+//     name: 'Bulbasaur',
+//     types: ['Grass', 'Poison'],
+//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png',
+//   },
+//   {
+//     id: 2,
+//     name: 'Ivysaur',
+//     types: ['Grass', 'Poison'],
+//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/002.png',
+//   },
+//   {
+//     id: 3,
+//     name: 'Venusaur',
+//     types: ['Grass', 'Poison'],
+//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png',
+//   },
+//   {
+//     id: 4,
+//     name: 'Charmander',
+//     types: ['Fire'],
+//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png',
+//   },
+//   {
+//     id: 5,
+//     name: 'Charmeleon',
+//     types: ['Fire'],
+//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/005.png',
+//   },
+//   {
+//     id: 6,
+//     name: 'Charizard',
+//     types: ['Fire', 'Flying'],
+//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/006.png',
+//   },
+//   {
+//     id: 7,
+//     name: 'Squirtle',
+//     types: ['Water'],
+//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png',
+//   },
+//   {
+//     id: 8,
+//     name: 'Wartortle',
+//     types: ['Water'],
+//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/008.png',
+//   },
+//   {
+//     id: 9,
+//     name: 'Blastoise',
+//     types: ['Water'],
+//     sprite: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/009.png',
+//   },
+// ];
 
 const FilteredPokedexTable = () => {
-  const [pokemon, setPokemon] = useState(pokemonArray);
+  // let defaultPokemon: TestPokemons[] = [];
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=20')
+      .then((response) => response.json())
+      .then((data: TestPokemon) => {
+        const pokemonUrls = data.results.map((result) => result.url);
+        return Promise.all(pokemonUrls.map((url) => fetch(url).then((response) => response.json())));
+      })
+      .then((detailedPokemon) => {
+        setPokemon(detailedPokemon);
+      });
+  }, []);
 
   const filterPokemon = (type: PokemonType | undefined) => {
     if (!type) {
-      setPokemon(pokemonArray);
-      return;
+      // setPokemon(pokemonArray);
+      // return;
     }
 
-    const filteredPokemon = pokemonArray.filter((p: Pokemon) => p.types.includes(type as PokemonType));
-    setPokemon(filteredPokemon);
+    // const filteredPokemon = pokemonArray.filter((p: Pokemon) => p.types.includes(type as PokemonType));
+    // setPokemon(filteredPokemon);
   };
 
   const byName = (a: Pokemon, b: Pokemon) => {
