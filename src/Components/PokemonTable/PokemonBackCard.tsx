1edfdb957b18e84087433da9ad74cc646ac9b31d
Felipe Flores <feliperflores2@hotmail.com>
2024-07-13 18:19:27 -0300

    feat(card): add base card interaction

---
+++ src/Components/PokemonTable/PokemonBackCard.tsx
@@ -0,0 +1,25 @@
+import { DetailedPokemonApi } from '../../PokemonTypes';
+import PokemonTypeFrame from './PokemonTypeFrame';
+
+type PokemonBackCardProps = {
+  pokemon: DetailedPokemonApi;
+};
+
+const PokemonBackCard = ({ pokemon }: PokemonBackCardProps) => (
+  <div className="min-w-56 group border-2 border-gray-300 rounded p-2 bg-gray-100">
+    <li className="text-start mb-1">
+      <span>#{pokemon.id}</span>
+    </li>
+    <li className="text-start mb-1">
+      <span className="font-semibold capitalize">{pokemon.name}</span>
+    </li>
+    <li className="flex items-center">
+      Types:{' '}
+      {pokemon.types.map((type) => (
+        <PokemonTypeFrame key={type.type.name} type={type.type.name} />
+      ))}
+    </li>
+  </div>
+);
+
+export default PokemonBackCard;
---
+++ src/Components/PokemonTable/PokemonFaceCard.tsx
@@ -0,0 +1,32 @@
+import { DetailedPokemonApi } from '../../PokemonTypes';
+import PokemonTypeFrame from './PokemonTypeFrame';
+
+type PokemonFaceCardProps = {
+  pokemon: DetailedPokemonApi;
+};
+
+const PokemonFaceCard = ({ pokemon: { id, name, types, sprites } }: PokemonFaceCardProps) => (
+  <div className="min-w-56 group border-2 border-gray-300 rounded p-2 bg-gray-100 transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-gray-300 hover:border-gray-400 duration-200">
+    <li className="flex justify-center">
+      <img
+        src={sprites.front_default || '/public/pokeball.svg'}
+        alt={name}
+        className="min-h-60 min-w-60 max-h-60 max-w-60 border rounded mb-2 bg-gray-200 transition duration-200 group-hover:bg-gray-400"
+      />
+    </li>
+    <li className="text-start mb-1">
+      <span>#{id}</span>
+    </li>
+    <li className="text-start mb-1">
+      <span className="font-semibold capitalize">{name}</span>
+    </li>
+    <li className="flex items-center">
+      Types:{' '}
+      {types.map((type) => (
+        <PokemonTypeFrame key={type.type.name} type={type.type.name} />
+      ))}
+    </li>
+  </div>
+);
+
+export default PokemonFaceCard;
