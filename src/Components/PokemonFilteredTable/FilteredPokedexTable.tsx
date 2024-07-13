eaf59a14d25b375e48af6e7bbb7a5bd7c9c9d2a8
Felipe Flores <feliperflores2@hotmail.com>
2024-07-12 23:31:49 -0300

    Add pagination

--- src/Components/PokemonFilteredTable/FilteredPokedexTable.tsx
+++ src/Components/PokemonFilteredTable/FilteredPokedexTable.tsx
@@ -1,24 +1,54 @@
-import { useEffect, useRef, useState } from 'react';
+import { Suspense, useEffect, useRef, useState } from 'react';
 import PokedexTable from '../PokemonTable/PokedexTable';
 import { DetailedPokemonApi, PokemonApi, PokemonType, SortOptions } from '../../PokemonTypes';
 import PokemonFilters from '../PokemonFilters/PokemonFilters';
 
+const fetchDetailedPokemon = (offset: number) =>
+  fetch(`https://pokeapi.co/api/v2/pokemon/?limit=21&offset=${offset}`)
+    .then((response) => response.json())
+    .then((data: PokemonApi) => {
+      const pokemonUrls = data.results.map((result) => result.url);
+      return Promise.all(pokemonUrls.map((pokeUrl) => fetch(pokeUrl).then((response) => response.json())));
+    });
+
+const byName = (a: DetailedPokemonApi, b: DetailedPokemonApi) => {
+  if (a.name > b.name) return 1;
+  if (a.name < b.name) return -1;
+  return 0;
+};
+const byId = (a: DetailedPokemonApi, b: DetailedPokemonApi) => {
+  const result = a.id - b.id;
+  if (result > 0) return 1;
+  if (result < 0) return -1;
+  return 0;
+};
+
+const SORT_BY = {
+  id: byId,
+  name: byName,
+};
+
+const PAGINATION_SIZE = 21;
+
 const FilteredPokedexTable = () => {
   const defaultPokemon = useRef<DetailedPokemonApi[]>([]);
-  const [pokemon, setPokemon] = useState<DetailedPokemonApi[]>(defaultPokemon.current);
+  const [pokemon, setPokemon] = useState<DetailedPokemonApi[]>([]);
+  const [offset, setOffset] = useState(0);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/?limit=20')
-      .then((response) => response.json())
-      .then((data: PokemonApi) => {
-        const pokemonUrls = data.results.map((result) => result.url);
-        return Promise.all(pokemonUrls.map((url) => fetch(url).then((response) => response.json())));
-      })
-      .then((detailedPokemon: DetailedPokemonApi[]) => {
-        defaultPokemon.current = detailedPokemon;
-        setPokemon(detailedPokemon);
-      });
-  }, []);
+    let ignore = false;
+
+    fetchDetailedPokemon(offset).then((detailedPokemon) => {
+      if (!ignore) {
+        defaultPokemon.current = [...defaultPokemon.current, ...detailedPokemon];
+        setPokemon(defaultPokemon.current);
+      }
+    });
+
+    return () => {
+      ignore = true;
+    };
+  }, [offset]);
 
   const filterPokemon = (type: PokemonType | undefined) => {
     if (!type) {
@@ -29,35 +59,28 @@ const FilteredPokedexTable = () => {
     const filteredPokemon = defaultPokemon.current.filter((p) => p.types.some((t) => t.type.name === type));
     setPokemon(filteredPokemon);
   };
+  const sortPokemonBy = (type: SortOptions) => {
+    setPokemon(pokemon.toSorted(SORT_BY[type]));
+  };
   const search = (name: string) => {
     const filteredPokemon = defaultPokemon.current.filter((p) => p.name.includes(name));
     setPokemon(filteredPokemon);
   };
-
-  const byName = (a: DetailedPokemonApi, b: DetailedPokemonApi) => {
-    if (a.name > b.name) return 1;
-    if (a.name < b.name) return -1;
-    return 0;
-  };
-  const byId = (a: DetailedPokemonApi, b: DetailedPokemonApi) => {
-    const result = a.id - b.id;
-    if (result > 0) return 1;
-    if (result < 0) return -1;
-    return 0;
-  };
-
-  const sortPokemonBy = (type: SortOptions) => {
-    const filters = {
-      id: byId,
-      name: byName,
-    };
-    setPokemon(pokemon.toSorted(filters[type]));
+  const paginate = () => {
+    setOffset(offset + PAGINATION_SIZE);
   };
 
   return (
     <div className="m-6">
-      <PokemonFilters selectType={filterPokemon} selectedType={undefined} sortBy={sortPokemonBy} search={search} />
-      <PokedexTable pokemonArray={pokemon} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PokemonFilters selectType={filterPokemon} selectedType={undefined} sortBy={sortPokemonBy} search={search} />
+        <PokedexTable pokemonArray={pokemon} />
+        <div className="flex w-full justify-center">
+          <button onClick={() => paginate()} type="button" className="bg-slate-100 border-2 rounded p-2">
+            Load More
+          </button>
+        </div>
+      </Suspense>
     </div>
   );
 };
--- src/Components/PokemonFilters/PokemonFilter.tsx
+++ src/Components/PokemonFilters/PokemonFilter.tsx
@@ -1,4 +1,5 @@
-import { PokemonType } from '../../PokemonTypes';
+import { useEffect, useState } from 'react';
+import { ApiStructure, PokemonType } from '../../PokemonTypes';
 import capitalizeFirstLetter from '../../Formatters/StringFormatter';
 
 type PokemonFilterProps = {
@@ -6,41 +7,33 @@ type PokemonFilterProps = {
   selectType: (type: PokemonType | undefined) => void;
 };
 
-const pokemonTypes: Readonly<PokemonType>[] = [
-  'bug',
-  'dark',
-  'dragon',
-  'electric',
-  'fairy',
-  'fighting',
-  'fire',
-  'flying',
-  'ghost',
-  'grass',
-  'ground',
-  'ice',
-  'poison',
-  'psychic',
-  'rock',
-  'steel',
-  'water',
-];
+const fetchTypes = () => fetch('https://pokeapi.co/api/v2/type/?limit=50').then((response) => response.json());
 
-const PokemonFilter = ({ selectedType, selectType }: PokemonFilterProps) => (
-  <>
-    <span>Type: </span>
-    <select
-      defaultValue={selectedType}
-      onChange={(event) => selectType((event.currentTarget.value as PokemonType) || undefined)}
-      className="p-1 border-2 rounded"
-    >
-      <option key="All" value={undefined} label="All" />
-      {pokemonTypes.map((type) => (
-        <option key={type} value={type}>
-          {capitalizeFirstLetter(type)}
-        </option>
-      ))}
-    </select>
-  </>
-);
+const PokemonFilter = ({ selectedType, selectType }: PokemonFilterProps) => {
+  const [types, setTypes] = useState<PokemonType[]>([]);
+  useEffect(() => {
+    fetchTypes().then((data) => {
+      setTypes(data.results.map((result: ApiStructure) => result.name));
+    });
+  }, []);
+
+  return (
+    <>
+      <span>Type: </span>
+      <select
+        defaultValue={selectedType}
+        onChange={(event) => selectType(event.currentTarget.value as PokemonType)}
+        className="p-1 border-2 rounded"
+      >
+        <option key="All" value={undefined} label="All" />
+        {types.map((type) => (
+          <option key={type} value={type}>
+            {capitalizeFirstLetter(type)}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+};
+
 export default PokemonFilter;
