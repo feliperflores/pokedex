9abcc71ceeac64c1b08a02840398c0748695e325
Felipe Flores <feliperflores2@hotmail.com>
2024-07-14 00:18:36 -0300

    feat(expand): start adding card flip animations

--- src/Components/PokemonTable/PokemonRow.tsx
+++ src/Components/PokemonTable/PokemonRow.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { DetailedPokemonApi } from '../../PokemonTypes';
 import PokemonFaceCard from './PokemonFaceCard';
 import PokemonBackCard from './PokemonBackCard';
@@ -9,12 +9,54 @@ type PokemonRowProps = {
 
 const PokemonRow = ({ pokemon }: PokemonRowProps) => {
   const [isVisible, setIsVisible] = useState(true);
+  const faceCard = useRef<HTMLDivElement>(null);
+  const backCard = useRef(null);
 
+  const flipCard = async () => {
+    if (faceCard?.current) {
+      await faceCard.current.animate(
+        [
+          {
+            transform: `
+              translate(50%)
+              rotateY(180deg)
+            `,
+          },
+          {
+            position: 'absolute',
+            transform: 'scale(2)',
+            // top: '50%',
+            // bottom: '50%'
+            left: '50%',
+            // margin: '0px -50px -50px -50px',
+            width: '100px',
+            height: '100px',
+          },
+        ],
+        {
+          duration: 1000,
+          easing: 'ease-in-out',
+        },
+      ).finished;
+    }
+    setIsVisible(!isVisible);
+  };
+  const unflipCard = () => {
+    setIsVisible(!isVisible); // aquele botao n vai ser necessario
+  };
   return (
-    <button type="button" className="m-3" onClick={() => setIsVisible(!isVisible)} aria-label="asd">
-      {isVisible && <PokemonFaceCard pokemon={pokemon} />}
-      {!isVisible && <PokemonBackCard pokemon={pokemon} />}
-    </button>
+    <>
+      {isVisible && (
+        <button aria-label="open details" type="button" className="m-3" onClick={() => flipCard()}>
+          <PokemonFaceCard ref={faceCard} pokemon={pokemon} />
+        </button>
+      )}
+      {!isVisible && (
+        <button aria-label="close details" type="button" onClick={() => unflipCard()}>
+          <PokemonBackCard ref={backCard} pokemon={pokemon} />
+        </button>
+      )}
+    </>
   );
 };
 
